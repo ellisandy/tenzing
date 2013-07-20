a1960552bc0134542dad207703b281f876a56c1c
Andy Ellis <andrew@andrewryanellis.com>
2013-07-20 10:08:58 -0500

    fixing smoke break bug.

--- app/models/smoke_break.rb
+++ app/models/smoke_break.rb
@@ -3,10 +3,12 @@ class SmokeBreak < ActiveRecord::Base
   
   belongs_to :user
   validates :user, :presence => true
+
+
+  def self.total_on(t = Time.now)
+         SmokeBreak.all(:conditions => ["created_at > ? AND created_at < ?", t.at_beginning_of_day, t.tomorrow.at_beginning_of_day])
+   end
   
-  def self.total_on(date)
-    where("date(created_at) = ?",date).count
-  end
 end
 # == Schema Information
 #
