d04f39ea83a62d51bfdd4e4d16989368bce5753d
Andy Ellis <andrew@andrewryanellis.com>
2013-08-20 08:50:19 -0500

    Reordering books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = current_user.books
+    @books = current_user.books.order("title")
 
     respond_to do |format|
       format.html # index.html.erb
