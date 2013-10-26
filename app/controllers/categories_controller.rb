2789eaa85b21791ad702fdc129d47e0ba0bdfdcf
Jack Ellis <jack@mnmlst.cc>
2013-10-26 11:46:01 -0700

    Fixing more UI

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,6 +14,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1.json
   def show
     @category = Category.find(params[:id])
+    @possessions = @category.possessions
 
     respond_to do |format|
       format.html # show.html.erb
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
   attr_accessible :title, :user_id
-
+  has_many :possessions
+  
   belongs_to :user
 end
