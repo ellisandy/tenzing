cc622974594881c398dbecd321998f73951bf02f
Jack Ellis <jack@mnmlst.cc>
2013-10-31 19:28:21 -0700

    Adding smokebreaks to the front page.

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -2,5 +2,6 @@ class LandingsController < ApplicationController
   def index
     @smoke_break = SmokeBreak.new
     @smoke_break_chart = current_user.smoke_breaks
+    @last_five_smoke_breaks = SmokeBreak.last(5)
   end
 end
