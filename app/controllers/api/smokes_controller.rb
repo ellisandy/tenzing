857686dc2f581ab630b1b4da8dad2259e5578c78
Jack Ellis <andellis@amazon.com>
2015-08-24 12:35:56 -0700

    first step for status board

--- app/controllers/api/smokes_controller.rb
+++ app/controllers/api/smokes_controller.rb
@@ -2,6 +2,38 @@ class API::SmokesController < ApplicationController
   before_filter :login_required, :except => [:new, :create]
   def index
     @smokes = SmokeBreak.all.count
+    
+    # @json_format = {
+    #   "graph" : {
+    #     "title" : "Smokes per Day"
+    #     "datasecuences" : [
+    #       {
+    #         "title": "Smokes",
+    #         "datapoints" : [
+    #           "title" : "today", "value" : "22",
+    #           "title" : "yesterday", "value" : "26",
+    #           "title" : "day before", "value" : "23"
+    #           ]
+    #       }
+    #     ]
+    #   }
+    # }
+
+    @smokes = { 
+      "graph" => { 
+        "title" => "Smokes per Day",
+        "datapoints" => [ 
+          {
+            "title" => "Smokes",
+            "datapoints" => [
+              {"title" => "today", "value" => "22"},
+              {"title" => "yesterday", "value" => "26"},
+              {"title" => "day before", "value" => "23"}
+            ]
+          }
+        ]
+      } 
+    }
     respond_to do |format|
       format.json {render :json => @smokes }
     end
