99918a940596b5e757e5496c1397c9c4b7484895
Jack Ellis <jack@mnmlst.cc>
2015-09-27 19:40:37 -0700

    Adding the date time

--- app/controllers/api/counts_controller.rb
+++ app/controllers/api/counts_controller.rb
@@ -3,9 +3,9 @@ class API::CountsController < ApplicationController
   def index
     @user = User.where(:api_key => params[:api_key])
     render json: 
-      "smokes:  #{@user.first.smoke_breaks.total_on(DateTime.now.in_time_zone("Pacific Time (US & Canada)"))} \n"+
-      "coffees: #{@user.first.coffee_intakes.total_on(DateTime.now.in_time_zone("Pacific Time (US & Canada)"))} \n"+
-      "drinks:  #{@user.first.drink_intakes.total_on(DateTime.now.in_time_zone("Pacific Time (US & Canada)"))}"
+      "smokes:  #{@user.first.smoke_breaks.total_on(DateTime.now.in_time_zone("Pacific Time (US & Canada)"))} (#{@user.smoke_breaks.last.recorded_time.in_time_zone("Pacific Time (US & Canada)").strftime("%B %e - %I:%M%p")})\n"+
+      "coffees: #{@user.first.coffee_intakes.total_on(DateTime.now.in_time_zone("Pacific Time (US & Canada)"))} (#{@user.coffee_intakes.last.recorded_time.in_time_zone("Pacific Time (US & Canada)").strftime("%B %e - %I:%M%p")})\n"+
+      "drinks:  #{@user.first.drink_intakes.total_on(DateTime.now.in_time_zone("Pacific Time (US & Canada)"))} (#{@user.drink_intakes.last.recorded_time.in_time_zone("Pacific Time (US & Canada)").strftime("%B %e - %I:%M%p")})"
   else
     return false
   end
