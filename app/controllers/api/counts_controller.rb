6b1c0763ccac55cea010bde23a3e897e8895104b
Andy Ellis <andrew@andrewryanellis.com>
2016-05-07 11:17:09 -0700

    Add support for Dashboard

--- app/controllers/api/counts_controller.rb
+++ app/controllers/api/counts_controller.rb
@@ -2,11 +2,6 @@ class API::CountsController < ApplicationController
   before_filter :login_required, :except => [:index]
   def index
     @user = User.where(:api_key => params[:api_key])
-    render json: 
-      "smokes:  #{@user.first.smoke_breaks.total_on(DateTime.now.in_time_zone("Pacific Time (US & Canada)"))} (#{@user.smoke_breaks.last.recorded_time.in_time_zone("Pacific Time (US & Canada)").strftime("%B %e - %I:%M%p")})\n"+
-      "coffees: #{@user.first.coffee_intakes.total_on(DateTime.now.in_time_zone("Pacific Time (US & Canada)"))} (#{@user.coffee_intakes.last.recorded_time.in_time_zone("Pacific Time (US & Canada)").strftime("%B %e - %I:%M%p")})\n"+
-      "drinks:  #{@user.first.drink_intakes.total_on(DateTime.now.in_time_zone("Pacific Time (US & Canada)"))} (#{@user.drink_intakes.last.recorded_time.in_time_zone("Pacific Time (US & Canada)").strftime("%B %e - %I:%M%p")})"
-  else
-    return false
+    render layout: false
   end
 end
